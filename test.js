8a8bd70552bf8845b946efde750fa1d6a15294a0
blibotics <blibotics@gmail.com>
2014-04-17 23:59:21 -0400

    Updated tests.js for clarity

--- test.js
+++ test.js
@@ -10,18 +10,17 @@ window.d = Z$('ul li');
 window.e = Z$('.article h1');
 window.f = Z$('#header span');
 
-window.ping = function() { console.log('ECHO!'); };
+window.ping = function() { console.log('Clicked the H1'); };
 
 b.on('click', 'h1', ping);
 a.on('click', function(e) {
-    console.log('sticky');
-
+    console.log('Clicked the #sticky');
 });
 
 b.on('click', 'a', function(e) {
     e.preventDefault();
     e.stopPropagation();
-    console.log('dsa');
+    console.log('Clicked a link in the body');
 });
 
 // window.c = Z$('article');
