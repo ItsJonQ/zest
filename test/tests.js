11e3a2ab27f184de98407b2c19bab7924a8674d8
ItsJonQ <itsjonq@gmail.com>
2014-05-02 23:31:02 -0400

    Fixed descriptions in classes test

--- test/tests.js
+++ test/tests.js
@@ -124,24 +124,24 @@ test("Zest().addClass should be able to add a single class", function() {
     expect(2);
 
     ok( span.addClass('new-guy'),
-        "Zest().addClass worked correctly." );
+        ".addClass worked correctly." );
 
     equal(
         span.getAttribute('class'),
         'new-guy',
-        "Zest().addClass added a new class correctly." );
+        ".addClass added a new class correctly." );
 });
 
 test("Zest().addClass should be able to add multiple classes", function() {
     expect(2);
 
     ok( span.addClass('new-guy is-cool very-cool'),
-        "Zest().addClass worked correctly." );
+        ".addClass worked correctly." );
 
     equal(
         span.getAttribute('class'),
         'new-guy is-cool very-cool',
-        "Zest().addClass added a new class correctly." );
+        ".addClass added a new class correctly." );
 });
 
 test("Zest().addClass is chainable", function() {
@@ -149,29 +149,29 @@ test("Zest().addClass is chainable", function() {
 
     ok( span.addClass('new-class')
         .removeClass('new-class'),
-        "Zest().addClass chaining is working." );
+        ".addClass chaining is working." );
 });
 
 
 // .hasClass
-test("Zest().rhasClass should be able detect classes", function() {
+test("Zest().hasClass should be able detect classes", function() {
     expect(3);
 
     // Adding a test class to the span to check for hasClass
     span.addClass('new-guy');
 
     ok( span.hasClass('new-guy'),
-        "Zest().hasClass worked correctly." );
+        ".hasClass worked correctly." );
 
     equal(
         span.hasClass('new-guy'),
         true,
-        "Zest().hasClass can detect a class correctly." );
+        ".hasClass can detect a class correctly." );
 
     equal(
         span.hasClass('fake-guy'),
         false,
-        "Zest().hasClass can detect a class is not present correctly." );
+        ".hasClass can detect a class is not present correctly." );
 });
 
 
@@ -183,12 +183,12 @@ test("Zest().removeClass should be able to remove a single class", function() {
     span.addClass('the new-guy too-cool');
 
     ok( span.removeClass('the'),
-        "Zest().removeClass worked correctly." );
+        ".removeClass worked correctly." );
 
     equal(
         span.hasClass('the'),
         false,
-        "Zest().removeClass removed a class correctly." );
+        ".removeClass removed a class correctly." );
 });
 
 test("Zest().removeClass should be able to remove multiple classes", function() {
@@ -201,12 +201,12 @@ test("Zest().removeClass should be able to remove multiple classes", function() 
     equal(
         span.hasClass('new-guy'),
         false,
-        "Zest().removeClass removed a class correctly." );
+        ".removeClass removed a class correctly." );
 
     equal(
         span.hasClass('too-cool'),
         false,
-        "Zest().removeClass removed a class correctly." );
+        ".removeClass removed a class correctly." );
 });
 
 test("Zest().removeClass is chainable", function() {
@@ -216,7 +216,7 @@ test("Zest().removeClass is chainable", function() {
         .addClass('new-class-one new-class-two')
         .removeClass('new-class-one')
         .removeClass('new-class-two'),
-        "Zest().removeClass chaining is working." );
+        ".removeClass chaining is working." );
 });
 
 /**
